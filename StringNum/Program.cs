dc501de2a3bc34ed73d5bce52681dfcb2426160f
Artur <artur071982@gmail.com>
2021-03-08 20:42:20 +0500

    load on

---
+++ StringNum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целые числа через пробел:\n");
+            string Num = Console.ReadLine();
+            string[] NumCh = Num.Split(new char[] { ' ' });//разделение строки по символам разделитель пробел
+            int SunNum = 0;
+            Console.WriteLine("\nРезультат суммы введеных чисел:\n");
+            for (int i = 0; i < NumCh.GetLength(0); i++)
+            {
+
+                SunNum = SunNum + Convert.ToInt32(NumCh[i]);
+                
+                //SStr = SStr.Insert(i,N);
+                if (i < NumCh.GetLength(0) - 1)
+                {
+                    Console.Write($"{NumCh[i]}+");
+                }
+                else
+                {
+                    Console.Write($"{NumCh[i]}={SunNum}");
+                }
+                
+
+            }
+            Console.ReadKey();
+        }
+        
+        
+
+    }
+}
+
